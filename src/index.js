4b06fdf03f29d4e991929bec4a1505a3e6b4448e
FeuerImBlut <golubovich.as@gmail.com>
2021-08-18 11:20:59 +0300

    Update index.js
    init: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (!Array.isArray(array) || !array.length) {
+		return 0;
+	}
+  array.sort(function(a,b) {
+	return a - b;  
+  }); 
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+		if (!Array.isArray(array) || !array.length) {
+		return 0;
+	}
+  array.sort(function(a,b) {
+	return b - a;  
+  }); 
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+		if (!Array.isArray(array) || !array.length) {
+		return 0;
+	}
+	var sum = 0;
+	array.forEach(element => sum = sum + element);
+	
+  return sum / array.length;
 }
